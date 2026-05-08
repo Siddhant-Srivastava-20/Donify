e99100aa374be13e3ffa0d7dabf2d297c746f6e0
Siddhant Srivastava <siddhantsrivastava20feb@gmail.com>
2026-05-09 04:18:39 +0530

    updating the functioning

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,3 +2,12 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Navbar from '../../components/Navbar/Navbar'
 import './Home.css'
+const Home = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Home
