329bc65b519f38317f97caeddd183d5c4bdd6840
Priyal Tayal <priyaltayal@Priyals-MacBook-Pro.local>
2026-05-09 02:02:42 +0530

    made some changes in navbar.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import MatchFinder from "./pages/MatchFinder/MatchFinder";
@@ -10,14 +9,13 @@ import DonorRegistry from "./pages/DonorRegistry/DonorRegistry";
 import DonorRegistration from "./pages/DonorRegistration/DonorRegistration";
 
 function App() {
-
   const [data, setData] = useState([]);
   const [donors, setDonors] = useState([]);
-
   useEffect(() => {
     fetch("https://backenderr.onrender.com/api/dataa")
       .then((response) => response.json())
       .then((result) => {
+        console.log(result);
         setData(result[0].requests);
         setDonors(result[0].donors);
       })
@@ -27,9 +25,8 @@ function App() {
   }, []);
 
   return (
-    <>
+    <Router>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/requests" element={<RequestBoard data={data} />} />
@@ -37,7 +34,7 @@ function App() {
         <Route path="/find-match" element={<MatchFinder />} />
         <Route path="/register" element={<DonorRegistration />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
