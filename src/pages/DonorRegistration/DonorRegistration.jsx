c123c785511e945bba7d964513ce3e1084a04c61
ashima-cyber <ashimab444@gmail.com>
2026-05-09 04:13:46 +0530

    "Create donor registration component"

--- src/pages/DonorRegistration/DonorRegistration.jsx
+++ src/pages/DonorRegistration/DonorRegistration.jsx
@@ -2,21 +2,7 @@ import { useState } from "react";
 import "./DonorRegistration.css";
 
 const DonorRegistration = () => {
-  // Form state - stores user input
-  const [formData, setFormData] = useState({
-    fullName: "",
-    email: "",
-    phone: "",
-    bloodGroup: "",
-    age: "",
-    city: "",
-  });
 
-  // Handle input changes
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  }};
+};
+
+export default DonorRegistration;
