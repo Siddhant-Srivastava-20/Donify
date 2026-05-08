a05b913821d0616c5747d53b1ecd062e31526340
Siddhant Srivastava <siddhantsrivastava20feb@gmail.com>
2026-05-09 03:45:14 +0530

    kldjs

---
+++ src/pages/MatchFinder/MatchFinder.jsx
@@ -0,0 +1,9 @@
+export default function MatchFinder() {
+    return (
+        <div className="page-container">
+            <div className="page-header">
+                <h1>Match Finder</h1>
+            </div>
+        </div>
+    );
+}
